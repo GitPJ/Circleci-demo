0ad7f2494e5be54994a95838ec7afa5887d6815d
dancong <823761027@qq.com>
2021-06-08 23:12:44 +0800

    app.vue Welcome to Your Vue.js App dancong->Welcome to Your Vue.js App dan

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App dancong"/>
+    <HelloWorld msg="Welcome to Your Vue.js App dan"/>
   </div>
 </template>
 
